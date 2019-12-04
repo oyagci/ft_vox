330a5f5214f2b9d44d6ac6151f9963fca0edff6c
Oguzhan YAGCI <oyagci@student.42.fr>
2019-12-04 17:34:23 +0100

    Add lazy::utils::Time with deltaTime calculations

---
+++ src/Time.hpp
@@ -0,0 +1,38 @@
+#include <GLFW/glfw3.h>
+
+namespace lazy {
+	namespace utils {
+		class Time {
+		public:
+			static Time &instance()
+			{
+				static Time t;
+
+				return t;
+			}
+
+		private:
+			Time() {};
+			Time(Time const &) = delete;
+			void operator=(Time const&) = delete;
+
+		public:
+			void update()
+			{
+				float currentFrame = glfwGetTime();
+
+				_deltaTime = currentFrame - _lastFrame;
+				_lastFrame = currentFrame;
+			}
+
+			static float getDeltaTime()
+			{
+				return instance()._deltaTime;
+			};
+
+		private:
+			float _deltaTime = 0.0f;
+			float _lastFrame = 0.0f;
+		};
+	}
+}
