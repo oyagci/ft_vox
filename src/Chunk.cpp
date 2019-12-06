abe4d271e98a1c32cd73ecf68a622cd618b28927
Oguzhan YAGCI <oyagci@student.42.fr>
2019-12-06 14:30:29 +0100

    Remove comments

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -26,28 +26,3 @@ Chunk::Block &Chunk::getBlock(std::size_t x, std::size_t y, std::size_t z)
 
 	return (*_blocks)[x * CHUNK_SIZE * CHUNK_SIZE + y * CHUNK_SIZE + z];
 }
-
-//void Chunk::initMesh()
-//{
-//	vec3 vertices[] = {
-//		vec3(0, 0,  0), vec3(1, 0,  0), vec3(1, 1,  0), vec3(0, 1,  0),
-//		vec3(0, 0, -1), vec3(1, 0, -1), vec3(1, 1, -1), vec3(0, 1, -1),
-//	};
-//	vec3 triangles[] = {
-//		vec3(0, 1, 2), vec3(0, 2, 3), // Front
-//		vec3(6, 5, 4), vec3(7, 6, 4), // Back
-//		vec3(0, 3, 7), vec3(0, 7, 4), // Left
-//		vec3(1, 5, 6), vec3(1, 6, 2), // Right
-//		vec3(3, 2, 6), vec3(3, 6, 7), // Top
-//		vec3(0, 5, 1), vec3(0, 4, 5), // Bottom
-//	};
-//
-//	_mesh = InstancedMesh(_shader);
-//	for (auto &vert : vertices) {
-//		_mesh.addPosition(std::move(vert));
-//	}
-//	for (auto &tri : triangles) {
-//		_mesh.addTriangle(std::move(tri));
-//	}
-//	_mesh.build();
-//}
