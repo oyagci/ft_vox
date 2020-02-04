929103165d18742f87a06b5f30357c9c2e4a72b5
Oguzhan YAGCI <oyagci@student.42.fr>
2020-02-04 18:09:31 +0100

    Add Texture class

---
+++ src/Texture.cpp
@@ -0,0 +1,39 @@
+#include "Texture.hpp"
+#include "stb_image.h"
+
+Texture::Texture(std::string const &name) : _name(name)
+{
+	glGenTextures(1, &_glId);
+}
+
+Texture::~Texture()
+{
+	glDeleteTextures(1, &_glId);
+}
+
+bool Texture::load(std::string const &path)
+{
+	unsigned char *data = stbi_load(path.c_str(), &_width, &_height, &_nChannel, 0);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0,
+			GL_RGB, _width, _height, 0,
+			GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		stbi_image_free(data);
+		return true;
+	}
+	return false;
+}
+
+void Texture::setParameter(GLenum target, GLenum param, GLenum value)
+{
+	glBindTexture(GL_TEXTURE_2D, _glId);
+	glTexParameteri(target, param, value);
+}
+
+void Texture::bind(GLuint textureNumber)
+{
+	glActiveTexture(textureNumber);
+	glBindTexture(GL_TEXTURE_2D, _glId);
+}
