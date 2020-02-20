14c9ada860aa9e1dcf8efdceb57c4c4fb97a141c
Oguzhan YAGCI <oyagci@student.42.fr>
2020-02-20 13:33:05 +0100

    Add action handlers to Game to prepare integration of menus

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,95 +1,107 @@
 #include "Game.hpp"
-#include <iostream>
-#include "lazy.hpp"
-#include "Chunk.hpp"
 #include "Time.hpp"
-#include "Chunk.hpp"
-#include "ChunkRenderer.hpp"
-#include "ChunkFactory.hpp"
-#include "TextRenderer.hpp"
-#include "FPSCounter.hpp"
-#include "World.hpp"
 #include "Settings.hpp"
 #include "stb_image.h"
-#include "PlayerHUD.hpp"
-
-using namespace lazy;
-using namespace graphics;
-using namespace inputs;
-using namespace utils;
 
 Game::Game()
 {
+	_state.game = PLAYING;
+
+	_display = std::make_unique<Display>("ft_vox", 1280, 720);
+	_display->enableCap(GL_DEPTH_TEST);
+	_display->enableCap(GL_CULL_FACE);
+	_display->enableCap(GL_BLEND);
+	_display->setFullscreen(true);
+
+	maths::transform t = { glm::vec3(32, 64, 32), glm::quat(), glm::vec3(1), nullptr };
+	_camera = std::make_unique<Camera>(*_display, t);
+	_camera->setProjection(glm::radians(80.0f), 0.1f, 1000.0f);
+
+	_world = std::make_unique<World>(*_camera);
+	_hud = std::make_unique<PlayerHUD>();
+	_fpsCounter = std::make_unique<FPSCounter>();
+	_textRenderer = std::make_unique<TextRenderer>();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDepthFunc(GL_LEQUAL);
 }
 
 int Game::run()
 {
 	Settings::instance().load("config.ini");
 
-	Display display("LazyGL", 1280, 720);
-	display.enableCap(GL_DEPTH_TEST);
-	display.enableCap(GL_CULL_FACE);
-	display.enableCap(GL_BLEND);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glDepthFunc(GL_LEQUAL);
-	display.setFullscreen(true);
-
-	stbi_set_flip_vertically_on_load(1);
-
-	TextRenderer tr;
-
-	glm::vec3 camPos(32, 64, 32);
-	maths::transform t = { camPos, glm::quat(), glm::vec3(1), nullptr };
-
-	Camera camera(display, std::move(t));
-	camera.setProjection(glm::radians(80.0f), 0.1f, 1000.0f);
-
-	World wr(camera);
-
 	FPSCounter fpsCounter;
 	PlayerHUD hud;
 
-	while (!display.isClosed())
+	while (!_display->isClosed())
 	{
 		Time::instance().update();
-		display.update();
-		display.updateInputs();
-		camera.update();
-
-		float deltaTime = Time::getDeltaTime();
-
-		fpsCounter.update(deltaTime);
-
-		float ms = 30.0f;
-		if (input::getKeyboard().getKey(GLFW_KEY_LEFT_SHIFT)) {
-			ms = 500.0f;
-		}
-		if (input::getKeyboard().getKey(GLFW_KEY_ESCAPE)) {
-			glfwSetWindowShouldClose(display.getWindow(), GLFW_TRUE);
-		}
-
-		camera.input(ms * deltaTime, 0.001f, {
-			GLFW_KEY_W,
-			GLFW_KEY_S,
-			GLFW_KEY_A,
-			GLFW_KEY_D,
-			GLFW_KEY_LEFT_CONTROL,
-			GLFW_KEY_SPACE
-		});
-
+		_display->update();
+		_display->updateInputs();
+		_camera->update();
+		_fpsCounter->update(Time::getDeltaTime());
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0x87 / 255.0f, 0xCE / 255.0f, 0xEB / 255.0f, 1.0f);
 
-		glDisable(GL_BLEND);
-		wr.update();
-		wr.render();
-		glEnable(GL_BLEND);
-		hud.draw();
+		switch (_state.game) {
+			case MAIN_MENU:
+				break ;
+			case PLAYING:
+				play();
+				break ;
+			case PAUSED:
+				break ;
+			default:
+				break ;
+		}
 
-		tr.drawText(std::to_string(fpsCounter.getFPS()) + " FPS", glm::vec2(10, 10), .3f, glm::vec3(1.0f));
 	}
 
 	return EXIT_SUCCESS;
+}
+
+void Game::action(GameAction action)
+{
+	switch (action) {
+		case PLAY:
+			_state.game = PLAYING;
+			break ;
+		case PAUSE:
+			_state.game = PAUSED;
+			break ;
+		default:
+			break ;
+	};
+}
+
+void Game::play()
+{
+	float deltaTime = Time::getDeltaTime();
+
+	float ms = 30.0f;
+	if (input::getKeyboard().getKey(GLFW_KEY_LEFT_SHIFT)) {
+		ms = 500.0f;
+	}
+	if (input::getKeyboard().getKey(GLFW_KEY_ESCAPE)) {
+		glfwSetWindowShouldClose(_display->getWindow(), GLFW_TRUE);
+	}
+
+	_camera->input(ms * deltaTime, 0.001f, {
+		GLFW_KEY_W,
+		GLFW_KEY_S,
+		GLFW_KEY_A,
+		GLFW_KEY_D,
+		GLFW_KEY_LEFT_CONTROL,
+		GLFW_KEY_SPACE
+	});
+
+	glDisable(GL_BLEND);
+	_world->update();
+	_world->render();
+	glEnable(GL_BLEND);
+	_hud->draw();
+
+	_textRenderer->drawText(std::to_string(_fpsCounter->getFPS()) + " FPS", glm::vec2(10, 10), .3f, glm::vec3(1.0f));
 }
--- src/Game.hpp
+++ src/Game.hpp
@@ -1,9 +1,49 @@
 #pragma once
 
+#include <memory>
+#include "lazy.hpp"
+#include "TextRenderer.hpp"
+#include "PlayerHUD.hpp"
+#include "FPSCounter.hpp"
+#include "World.hpp"
+
+using namespace lazy;
+using namespace graphics;
+using namespace inputs;
+using namespace utils;
+
 class Game
 {
+private:
+	enum StateVal {
+		MAIN_MENU,
+		PLAYING,
+		PAUSED,
+	};
+
+	enum GameAction {
+		PLAY,
+		PAUSE,
+	};
+
+	struct GameState {
+		StateVal game;
+	};
 public:
 	Game();
 
 	int run();
+
+private:
+	void action(GameAction action);
+	void play();
+	void mainMenu();
+
+	std::unique_ptr<Display> _display;
+	std::unique_ptr<TextRenderer> _textRenderer;
+	std::unique_ptr<Camera> _camera;
+	std::unique_ptr<World> _world;
+	std::unique_ptr<PlayerHUD> _hud;
+	std::unique_ptr<FPSCounter> _fpsCounter;
+	GameState _state;
 };
